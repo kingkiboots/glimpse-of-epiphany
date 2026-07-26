82568d9a9f253d8032c6db9a7954fc87de6dbd2c
kingkiboots <tuffhr4@gmail.com>
2026-07-26 21:44:31 +0900

    feat: index 페이지 추가

--- apps/mobile-web/src/pages/home/ui/HomePage.tsx
+++ apps/mobile-web/src/pages/home/ui/HomePage.tsx
@@ -1,5 +1,29 @@
+import Typography from "@/shared/ui/Typography";
+import styles from "./HomePage.module.css";
+import Button from "@/shared/ui/Button";
+
 const HomePage = () => {
-  return <p>모바일 웹 홈 화면</p>;
+  return (
+    <section className={styles.container}>
+      <div className={styles.textArea}>
+        <Typography as={"h1"} variant="title">
+          일상 속 감사 찾기
+        </Typography>
+        <Typography as={"p"} variant="body">
+          최근 찍은 사진을 천천히 돌아보며
+          <br />
+          하나님께 감사하게 되는
+          <br />한 장의 사진을 선택해 보세요.
+        </Typography>
+      </div>
+      <div className={styles.ctaArea}>
+        <Button>일상 속 감사 찾기</Button>
+        <Typography as={"p"} variant="caption" className={styles.ctaCaption}>
+          선택 후 바로 공유되지 않습니다.
+        </Typography>
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
