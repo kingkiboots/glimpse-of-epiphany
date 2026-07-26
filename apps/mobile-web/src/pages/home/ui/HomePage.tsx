2f2399b7be3dd7ec987846fd743e4435771b2bba
kingkiboots <tuffhr4@gmail.com>
2026-07-27 00:12:15 +0900

    fix: html 구조 변경

--- apps/mobile-web/src/pages/home/ui/HomePage.tsx
+++ apps/mobile-web/src/pages/home/ui/HomePage.tsx
@@ -1,6 +1,5 @@
 import { ROUTE_PATHS } from "@/shared/consts";
 import Button from "@/shared/ui/Button";
-import Typography from "@/shared/ui/Typography";
 import { useNavigate } from "@tanstack/react-router";
 import styles from "./HomePage.module.css";
 
@@ -12,30 +11,23 @@ const HomePage = () => {
   };
 
   return (
-    <div className={styles.container}>
+    <div className={styles.content}>
       <div className={styles.textArea}>
-        <Typography as={"h1"} variant="title">
-          일상 속 감사 찾기
-        </Typography>
-        <Typography as={"p"} variant="body">
+        <h1 className={styles.title}>일상 속 감사 찾기</h1>
+        <p className={styles.body}>
           최근 찍은 사진을 천천히 돌아보며
           <br />
           하나님께 감사하게 되는
           <br />한 장의 사진을 선택해 보세요.
-        </Typography>
+        </p>
       </div>
       <div className={styles.ctaArea}>
         <Button aria-describedby="cta-caption" onClick={handleCtaClick}>
           일상 속 감사 찾기
         </Button>
-        <Typography
-          as={"p"}
-          variant="caption"
-          id="cta-caption"
-          className={styles.ctaCaption}
-        >
+        <p id="cta-caption" className={styles.ctaCaption}>
           선택 후 바로 공유되지 않습니다.
-        </Typography>
+        </p>
       </div>
     </div>
   );
--- apps/mobile-web/src/pages/preparing/ui/PreparingPage.tsx
+++ apps/mobile-web/src/pages/preparing/ui/PreparingPage.tsx
@@ -1,31 +1,34 @@
-import Typography from "@/shared/ui/Typography";
-import styles from "./PreparingPage.module.css";
-import { useState } from "react";
 import { getRandomBibleVerse } from "@/features/bible-verse/consts";
 import Spinner from "@/shared/ui/Spinner";
+import { useEffect, useState } from "react";
+import styles from "./PreparingPage.module.css";
+import { useNavigate } from "@tanstack/react-router";
+import { ROUTE_PATHS } from "@/shared/consts";
 
 const PreparingPage = () => {
   const [bibleVerse] = useState(getRandomBibleVerse);
   const { book, chapter, verse, text } = bibleVerse;
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate({ to: ROUTE_PATHS.compose, replace: true });
+    }, 5000);
+  }, []);
+
   return (
-    <div className={styles.container}>
-      <div className={styles.textArea}>
-        <Typography as={"h1"} variant="title">
-          일상 속 감사 찾기
-        </Typography>
+    <div className={styles.content}>
+      <h1 className={styles.title}>일상 속 감사 찾기</h1>
+      <div className={styles.loading}>
+        <Spinner size={48} />
+        <span className={styles.loadingLabel}>Loading...</span>
       </div>
-      <Spinner size={48} />
-      <div className={styles.bibleVerseArea}>
-        <Typography as={"p"} variant="caption">
+      <div className={styles.verse}>
+        <p className={styles.verseReference}>
           {book} {chapter}:{verse}
-        </Typography>
-        <Typography
-          as={"p"}
-          variant="caption"
-          className={styles.bibleVerseContent}
-        >
-          {text}
-        </Typography>
+        </p>
+        <p className={styles.verseText}>{text}</p>
       </div>
     </div>
   );
