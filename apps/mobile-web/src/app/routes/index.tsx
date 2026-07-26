fee5cece9a85213377a764c9731d96ea9d1a8ff7
kingkiboots <tuffhr4@gmail.com>
2026-07-26 22:30:05 +0900

    fix: routeTree.gen.ts 관련 빌드 에러 수정
    build를 돌린 다음에 routeTree.gen.ts 파일이 생성된다. 이것까지 만든 다음에 tsc -b 돌릴 수 있게  스크립트 실행 순서 변경

--- apps/mobile-web/src/app/routes/index.tsx
+++ apps/mobile-web/src/app/routes/index.tsx
@@ -1,6 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router'
-import HomePage from '@/pages/home'
+import { createFileRoute } from "@tanstack/react-router";
+import HomePage from "@/pages/home";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: HomePage,
-})
+});
