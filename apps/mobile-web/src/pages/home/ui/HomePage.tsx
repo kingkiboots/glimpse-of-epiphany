17538ddc345c532cbcee6f55087b266ba56ae369
kingkiboot <99400369+kingkiboots@users.noreply.github.com>
2026-07-27 01:38:11 +0000

    fix(ui): HomePage 레이아웃 수정

--- apps/mobile-web/src/pages/home/ui/HomePage.tsx
+++ apps/mobile-web/src/pages/home/ui/HomePage.tsx
@@ -12,7 +12,7 @@ const HomePage = () => {
 
   return (
     <div className={styles.content}>
-      <div className={styles.textArea}>
+      <header className={styles.textArea}>
         <h1 className={styles.title}>일상 속 감사 찾기</h1>
         <p className={styles.body}>
           최근 찍은 사진을 천천히 돌아보며
@@ -20,7 +20,7 @@ const HomePage = () => {
           하나님께 감사하게 되는
           <br />한 장의 사진을 선택해 보세요.
         </p>
-      </div>
+      </header>
       <div className={styles.ctaArea}>
         <Button aria-describedby="cta-caption" onClick={handleCtaClick}>
           일상 속 감사 찾기
