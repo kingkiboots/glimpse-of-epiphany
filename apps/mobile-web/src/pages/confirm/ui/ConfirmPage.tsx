b81b460862aca00d3c112d474b73ae4595db6340
kingkiboot <99400369+kingkiboots@users.noreply.github.com>
2026-07-27 09:08:35 +0000

    feat: 컨펌 페이지 추가

--- apps/mobile-web/src/pages/confirm/ui/ConfirmPage.tsx
+++ apps/mobile-web/src/pages/confirm/ui/ConfirmPage.tsx
@@ -1,5 +1,8 @@
 import Button from "@/shared/ui/Button";
 import Panel from "@/shared/ui/Panel";
+import { getCurrentDate } from "@packages/utils";
+import { useCanGoBack, useRouter } from "@tanstack/react-router";
+import { useState } from "react";
 import styles from "./ConfirmPage.module.css";
 
 type ConfirmPageProps = {
@@ -7,44 +10,74 @@ type ConfirmPageProps = {
   message?: string;
 };
 
-const ConfirmPage = ({ photoUrl = null, message = "" }: ConfirmPageProps) => {
+const ConfirmPage = ({
+  photoUrl = null,
+  message = `감사한 날이었다~~\n너무 좋다~~~~\n행복하고 하나님 너무 좋다~~~~\n조금 안감사했지만 돌아보니 감사하다`,
+}: ConfirmPageProps) => {
+  const [currentDate] = useState(getCurrentDate);
+
+  const router = useRouter();
+  const canGoBack = useCanGoBack();
+
+  const handlePrevStepClick = () => {
+    if (!canGoBack) {
+      return;
+    }
+
+    router.history.back();
+  };
+
   return (
     <div className={styles.content}>
-      <header className={styles.textArea}>
+      <header className={styles.header}>
         <h1 className={styles.title}>일상 속 감사 찾기</h1>
       </header>
-      <div className={styles.imageBoxArea}>
-        <Panel radius="panel" padding="8px" width={272} height={247}>
-          <div className={styles.imagePreview}>
-            {photoUrl ? (
-              <img
-                src={photoUrl}
-                alt="선택한 사진"
-                className={styles.previewImage}
-              />
-            ) : (
-              <span className={styles.placeholderLabel}>
-                선택한 사진이 없어요
-              </span>
-            )}
+      <div className={styles.previewContainer}>
+        <Panel
+          radius="panel"
+          padding="11px"
+          width={272}
+          height={388}
+          style={{
+            maxHeight: "388px",
+          }}
+        >
+          <div className={styles.previewArea}>
+            <div className={styles.imagePreview}>
+              {photoUrl ? (
+                <img
+                  src={photoUrl}
+                  alt="선택한 사진"
+                  className={styles.previewImage}
+                />
+              ) : (
+                <span className={styles.placeholderLabel}>
+                  선택한 사진이 없어요
+                </span>
+              )}
+            </div>
+            <div className={styles.messagePreview}>
+              <p className={styles.previewMessage}>{message}</p>
+              <p className={styles.previewMessageCaption}>
+                {currentDate}
+                <br /> 삶으로 쓰는 예배전 (展)
+              </p>
+            </div>
           </div>
-        </Panel>
-      </div>
-      <div className={styles.inputArea}>
-        <p className={styles.label}>작성한 감사 내용</p>
-        <Panel radius="panel" padding="10px 12px" width={272} height={134}>
-          <p className={styles.messagePreview}>
-            {message || "작성한 문구가 없어요"}
-          </p>
         </Panel>
       </div>
       <div className={styles.ctaArea}>
         <p id="cta-caption" className={styles.ctaCaption}>
-          *공유하면 빔프로젝터 화면에 바로 표시돼요.
+          *내용은 공유되지 않습니다.
         </p>
-        <Button aria-describedby="cta-caption" onClick={() => {}}>
-          공유하기
-        </Button>
+        <div className={styles.ctaButtonGroup}>
+          <Button aria-describedby="cta-caption" onClick={() => {}}>
+            이미지 전시하기
+          </Button>
+          {/* STUB - 이 기능은 없어짐 */}
+          {/* <Button onClick={() => {}}>저장하기</Button> */}
+          <Button onClick={handlePrevStepClick}>이전으로</Button>
+        </div>
       </div>
     </div>
   );
